09c337d226290e7001561b159e468f70355aaabb
Dan Serban <dan.serban@ejobsgroup.ro>
2020-05-15 12:57:28 +0300

    fix AUTO_IP_RANGE detection

--- scripts/autodiscover.sh
+++ scripts/autodiscover.sh
@@ -7,8 +7,5 @@ SUGGESTED_LAST_OCTET_OF_IP="66" # no checks for this
 AUTO_JAIL_IP=$(awk -F"." '{print $1"."$2"."$3".'${SUGGESTED_LAST_OCTET_OF_IP}'"}'<<<${AUTO_GW_IP})
 AUTO_RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g" | sed "s/-p.*//g")
 AUTO_EXT_IP=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-AUTO_IP_RANGE=$(awk -F"." '{print $1"."$2"."$3".'0'"}'<<<${AUTO_GW_IP})
-# AUTO_IP_RANGE=$(netstat -rn | grep -E "U[^A-Z]" | grep -v lo0 | awk '{print $1;}' | sed "s/\/.*//g") # 192.168.1.0
-echo AUTO_IP_RANGE ${AUTO_IP_RANGE}
-exit 0
+AUTO_IP_RANGE=$(awk -F"." '{print $1"."$2"."$3".'0'"}'<<<${AUTO_GW_IP}) # 192.168.1.0
 
