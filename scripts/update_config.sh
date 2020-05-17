fc4882678dc2f387ec1285a1a724526a95fff8ff
Dan Serban <dan.serban@ejobsgroup.ro>
2020-05-18 01:05:27 +0300

    z

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -12,7 +12,7 @@ echo
 
 # Add 'EXT_IP' variable to let usage of DDNS
 grep -q 'EXT_IP' ${FVARS} || sed -i "" "/JAIL_NAME/a \\
-
+\n
 # External IP/DDNS that you'll use to connect \
 # Default: your-IP \
 EXT_IP=\"\" \
